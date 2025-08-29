8fc230fd840f3c0786f5c9958fa8c5f7bab3f6c8
vichoko <vicenteoyanedel@gmail.com>
2025-08-29 14:46:07 -0400

    style: ruff

--- src/tibia_hunts/wsgi.py
+++ src/tibia_hunts/wsgi.py
@@ -11,8 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+
 # Use production settings when running on Google App Engine
-if os.getenv('GAE_APPLICATION', None):
+if os.getenv("GAE_APPLICATION", None):
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tibia_hunts.settings_production")
 else:
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tibia_hunts.settings")
